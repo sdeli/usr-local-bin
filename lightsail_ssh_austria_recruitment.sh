663f45f657c9ea5efce4104f4f5ccd9f3edd3eef
sdeli <bgfkszmsdeli@gmail.com>
2020-08-31 10:42:49 +0200

    feat(ssh): austria recruitment lightsail ssh

---
+++ lightsail_ssh_austria_recruitment.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+ssh -i  /Users/sandordeli/Projects/ssh-keys/LightsailDefaultKey-eu-central-1.pem bitnami@18.157.103.85
--- lightsail_ssh_prn.sh
+++ lightsail_ssh_prn.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ssh -i /Users/sandordeli/Documents/LightsailDefaultKey-eu-central-1.pem bitnami@18.184.208.138
+ssh -i /Users/sandordeli/Projects/ssh-keys/LightsailDefaultKey-eu-central-1.pem bitnami@18.184.208.138
