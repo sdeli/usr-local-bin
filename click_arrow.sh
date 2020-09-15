b7e3ca486e2e487532f1fbb0c1724db9fd683e1c
sandordeli <sandor.deli@cloudstorm.io>
2020-09-15 13:11:42 +0200

    feat(click_arrow.sh): shortcut with arrow script

---
+++ click_arrow.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+open -a /Applications/create-screenshot-open-in-preview.app
+sleep 1.5
+/Users/sandordeli/bin/cliclick c:1221,58
+sleep 0.2
+/Users/sandordeli/bin/cliclick c:205,98
+sleep 0.2
+/Users/sandordeli/bin/cliclick c:209,139
+sleep 0.2
+/Users/sandordeli/bin/cliclick m:787,580
