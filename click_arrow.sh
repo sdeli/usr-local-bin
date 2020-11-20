abea8bb6d233cc90d86cf89adc9e004b1e53397e
sandordeli <sandor.deli@cloudstorm.io>
2020-11-20 22:20:16 +0100

    feat(new-scripts): done

--- click_arrow.sh
+++ click_arrow.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
-open -a /Applications/create-screenshot-open-in-preview.app
-sleep 1.5
+DATE=$(date '+%Y-%m-%d_%H-%M-%S')
+SCREENSHOT_FILE_NAME=screenshot_"${DATE}"
+SCREENSHOT_FILE_PATH=/Users/sandordeli/Documents/screenshots/"${SCREENSHOT_FILE_NAME}"
+
+screencapture -t jpg $SCREENSHOT_FILE_PATH
+open -a Preview $SCREENSHOT_FILE_PATH
+
+FRONT_WINDOW_NAME=`osascript -e "tell application \"Preview\" to get the name of front window"`
+
+if [ "$SCREENSHOT_FILE_NAME" != "$FRONT_WINDOW_NAME" ]; then
+  osascript -e "say \"something has gone here wrong\" using \"Victoria\""
+  exit 1
+fi
+
+sleep 0.5
 /Users/sandordeli/bin/cliclick c:1221,58
 sleep 0.2
 /Users/sandordeli/bin/cliclick c:205,98
--- open_github_page.sh
+++ open_github_page.sh
@@ -1,3 +1,3 @@
-STAMM="$(git config --get remote.origin.url | sed 's/\  .git//g')"
+STAMM="$(git config --get remote.origin.url | sed 's/\.git//g')"
 BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
 open "${STAMM}"/tree/"${BRANCH_NAME}"
